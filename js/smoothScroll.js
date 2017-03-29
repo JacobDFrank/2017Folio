b61828ca63c3685d3a463063a6de95252ccac27b
JacobDFrank <frankjacob42@gmail.com>
2017-03-29 14:04:28 -0400

    Worked on case page

---
+++ js/smoothScroll.js
@@ -0,0 +1,35 @@
+document.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+});
+var section = document.querySelector(".section");
+
+var sectionHeight = section.getBoundingClientRect().height;
+
+VirtualScroll.on(function(e) {
+    // e is an object that holds scroll values, including:
+    e.deltaY; // <- amount of pixels scrolled vertically since last call
+    e.deltaX; // <- amount of pixels scrolled horizontally since last call
+});
+
+var targetY = 0;
+
+VirtualScroll.on(function(e) {
+    targetY += e.deltaY;
+    targetY = Math.max( ( window.innerHeight * 2) * -1, targetY);
+    targetY = Math.min(0, targetY);
+});
+
+var currentY = 0, ease = 0.1;
+
+var run = function() {
+    requestAnimationFrame(run);
+    currentY += (targetY - currentY) * ease;
+    var t = 'translateY(' + currentY + 'px) translateZ(0)';
+    var s = section.style;
+    s["transform"] = t;
+    s["webkitTransform"] = t;
+    s["mozTransform"] = t;
+    s["msTransform"] = t;
+}
+
+run();
