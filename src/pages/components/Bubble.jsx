9e8fa60c764d4ca318b33e8b4616779022b91572
JacobDFrank <frankjacob42@gmail.com>
2017-12-27 00:07:32 -0500

    redid script and finished new load

--- src/pages/components/Bubble.jsx
+++ src/pages/components/Bubble.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const testImage = '/assets/empty.gif';
-// const scriptURL = '/assets/bubble.js';
-const scriptURL = '/assets/uncompiled.js';
+const scriptURL = '/assets/bubble.js';
+// const scriptURL = '/assets/uncompiled.js'; for testing purposes
 
 class Bubble extends React.Component {
   componentDidMount() {
